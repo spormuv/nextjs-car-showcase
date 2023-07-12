84fdaddf292bbeebb957a0f819d77bcb1db85003
Mike <spormuv@gmail.com>
2023-07-12 21:38:51 +0300

    feat: add combobox

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,5 @@
+const CustomFilter = () => {
+  return <div>customFilter</div>;
+};
+
+export default CustomFilter;
--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,8 @@
 import CustomButton from './CustomButton';
+import CustomFilter from './CustomFilter';
 import Footer from './Footer';
 import Hero from './Hero';
 import Navbar from './Navbar';
+import SearchBar from './SearchBar';
 
-export { CustomButton, Footer, Hero, Navbar };
+export { CustomButton, CustomFilter, Footer, Hero, Navbar, SearchBar };
